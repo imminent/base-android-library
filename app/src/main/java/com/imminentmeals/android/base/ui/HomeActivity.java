f345f6664e2ccb0fca46138a0689cf6c1b6f3657
Dandre Allison <dandre.allison@gmail.com>
2013-03-20 16:08:27 -0700

    Corrects Home Screen title.

--- app/src/main/java/com/imminentmeals/android/base/ui/HomeActivity.java
+++ app/src/main/java/com/imminentmeals/android/base/ui/HomeActivity.java
@@ -1,12 +1,23 @@
 package com.imminentmeals.android.base.ui;
 
+import javax.annotation.CheckForNull;
+import javax.annotation.OverridingMethodsMustInvokeSuper;
+
+import android.os.Bundle;
+
+import com.imminentmeals.android.base.R;
 import com.imminentmeals.android.base.ui.base.BaseActivity;
 
 /**
  * <p>Controller that provides the Home screen.</p>
- *
  * @author Dandré Allison
  */
 public class HomeActivity extends BaseActivity {
 
+    @Override
+    @OverridingMethodsMustInvokeSuper
+    protected void onCreate(@CheckForNull Bundle icicle) {
+        super.onCreate(icicle);
+        getSupportActionBar().setTitle(R.string.title_home_screen);
+    }
 }
