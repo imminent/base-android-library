54aa55407ea878c596d650f2fd58adde2e2dafdf
Dandre Allison <dandre.allison@gmail.com>
2013-11-07 12:01:04 -0800

    Adds ButterKnife to InjectionCallbacks.

--- app/src/main/java/com/imminentmeals/android/base/activity_lifecycle_callbacks/InjectionCallbacks.java
+++ app/src/main/java/com/imminentmeals/android/base/activity_lifecycle_callbacks/InjectionCallbacks.java
@@ -1,13 +1,12 @@
 package com.imminentmeals.android.base.activity_lifecycle_callbacks;
 
-import javax.annotation.CheckForNull;
-import javax.inject.Inject;
-
 import android.app.Activity;
 import android.os.Bundle;
-
+import butterknife.Views;
 import com.imminentmeals.android.base.utilities.ObjectGraph;
 import com.imminentmeals.android.base.utilities.SimpleActivityLifecycleCallbacks;
+import javax.annotation.CheckForNull;
+import javax.inject.Inject;
 
 /**
  * <p>Injects the {@link Activity} with its dependencies.</p>
@@ -21,5 +20,6 @@ public class InjectionCallbacks extends SimpleActivityLifecycleCallbacks {
     @Override
     public void onActivityCreated(Activity activity, @CheckForNull Bundle icicle) {
         ObjectGraph.inject(activity);
+        Views.inject(activity);
     }
 }
